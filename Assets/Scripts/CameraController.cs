c69865fee0cd026fa7f3e3beaca3615d5530557e
BlueTitanium <xyzftw@gmail.com>
2019-09-10 22:11:13 -0400

    Basic Camera and Player Movement

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour{
+    public Transform target;
+
+    public float smooth = .125f;
+    public Vector3 off;
+    private Vector3 vel = Vector3.zero;
+    // Start is called before the first frame update
+    void Start(){}
+
+    // Update is called once per frame
+    void FixedUpdate(){
+        Vector3 desPos = target.position + off;
+        Vector3 smoothPos = Vector3.SmoothDamp(transform.position, desPos, ref vel,smooth, 2f);
+        transform.position = smoothPos;
+        transform.LookAt(target.position);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rb3d;
+    
+    public float speed = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb3d = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xVal = Input.GetAxis("Horizontal");
+        float zVal = Input.GetAxis("Vertical");
+        transform.Translate(new Vector3(xVal, 0, zVal)*speed);
+    }
+}
