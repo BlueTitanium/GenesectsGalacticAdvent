606cb50d5624d901b89ae9406be12b0b8dbaeb07
BlueTitanium <xyzftw@gmail.com>
2019-09-15 17:45:02 -0400

    Implemented Celestial Rotation

---
+++ Assets/Scripts/SolarSystem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SolarSystem : MonoBehaviour
+{
+    public float speed = 6f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        TimeSpan time = DateTime.Now.TimeOfDay;
+        transform.localRotation = Quaternion.Euler(0f, (float)time.TotalMinutes * speed, 0f);
+    }
+}
