1df612b3451bb7760e2d5689fc2abd6aa333c69b
BlueTitanium <xyzftw@gmail.com>
2019-09-18 18:53:09 -0400

    Working Ground Detection

---
+++ Assets/Scripts/GroundCheck.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundCheck : MonoBehaviour
+{
+    public bool grounded;
+    public LayerMask layer;
+    void Update()
+    {
+        if(!Physics.CheckCapsule(GetComponent<Collider>().bounds.center,
+                                new Vector3(GetComponent<Collider>().bounds.center.x,
+                                GetComponent<Collider>().bounds.min.y-0.1f,
+                                GetComponent<Collider>().bounds.center.z),
+                                0.25f,
+                                layer))
+            grounded = false;
+        else grounded = true;
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,21 +8,35 @@ public class PlayerController : MonoBehaviour
     private Rigidbody rb3d;
     public float sensitivity;
     public float speed = 1f;
+    public bool grounded;
+    public GameObject leftFoot;
+    public GameObject rightFoot;
+    private bool leftGround;
+    private bool rightGround;
 
     // Start is called before the first frame update
     void Start()
     {
         rb3d = GetComponent<Rigidbody>();
         offset = new Vector3(0,0,0);
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+        leftGround = leftFoot.GetComponent<GroundCheck>().grounded;
+        rightGround = rightFoot.GetComponent<GroundCheck>().grounded;
+        
+        if(leftGround || rightGround) grounded = true;
+        else grounded = false;
+
         transform.Rotate(0, sensitivity * Input.GetAxis("Mouse X"),0);
         
         float xVal = Input.GetAxis("Horizontal");
         float zVal = Input.GetAxis("Vertical");
+
         transform.Translate(new Vector3(xVal, 0, zVal)*speed);
     }
+
 }
