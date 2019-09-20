13d466ef79ea39bb1c3e27de22af3608f0fa2348
BlueTitanium <xyzftw@gmail.com>
2019-09-19 23:33:27 -0400

    Flying implemented, and brightness intensity fixed

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,14 +7,15 @@ public class PlayerController : MonoBehaviour
     private Vector3 offset;
     private Rigidbody rb3d;
     public float sensitivity;
-    public float speed = 1f;
+    public float groundSpeed = 1f;
+    public float flySpeed = 1f;
     public float jumpForce = 20f;
     public bool grounded;
     public GameObject leftFoot;
     public GameObject rightFoot;
     private bool leftGround;
     private bool rightGround;
-
+    public bool flying = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -37,11 +38,19 @@ public class PlayerController : MonoBehaviour
         float xVal = Input.GetAxis("Horizontal");
         float zVal = Input.GetAxis("Vertical");
 
-        transform.Translate(new Vector3(xVal, 0, zVal)*speed);
+        if(!flying) transform.Translate(new Vector3(xVal, 0, zVal) * groundSpeed);
 
         float yMove = 0;
         if(Input.GetKey("space") && grounded) yMove = 1;
-
         rb3d.AddForce(0, jumpForce*yMove, 0);
+
+        if(Input.GetKey("space") && !grounded && !flying){flying = true;rb3d.velocity = new Vector3(0, 0, 0);}
+        if(flying) {
+            rb3d.useGravity = false;
+            if(Input.GetKey("space")) yMove = 1;
+            if(Input.GetKey("left shift")) yMove = -1;
+            transform.Translate(new Vector3(xVal, yMove, zVal) * flySpeed);
+        }
+        if(grounded && flying) {flying = false;rb3d.useGravity= true;}
     }
 }
