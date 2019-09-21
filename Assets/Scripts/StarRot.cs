8c1171fd8ffb2cd5e1b615c60045ba2a1ec5c94b
BlueTitanium <xyzftw@gmail.com>
2019-09-21 17:07:48 -0400

    Added Star Rotation

---
+++ Assets/Scripts/StarRot.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class StarRot : MonoBehaviour
+{
+    public float x;
+    public float z;
+    public float speed;
+    private float time = 0;
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+        transform.rotation = Quaternion.Euler(x, (float)time * speed, z);
+    }
+}
