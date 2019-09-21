0c1221b2f2637bd714db9b50880c54958cf9d531
BlueTitanium <xyzftw@gmail.com>
2019-09-21 19:10:35 -0400

    Created Good Obstacles

---
+++ Assets/Scripts/ObstacleCreator.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ObstacleCreator : MonoBehaviour
+{
+    public GameObject fab;
+    public float radius = .1f;
+    private GameObject obj;
+    // Start is called before the first frame update
+    void Start()
+    {
+        obj = Instantiate(fab, transform) as GameObject;
+        obj.transform.SetParent(transform, false);
+        obj.transform.localPosition = new Vector3(0,0,0);
+   }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
